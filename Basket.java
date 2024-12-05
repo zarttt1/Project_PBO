99222690b66a831170a4f20ccd2ef3725caa6924
N4thhh <130668865+N4thhh@users.noreply.github.com>
2024-12-05 19:27:29 +0700

    Create Basket.java
    class basket untuk menyimpan kecepatan pergeraka dan nyawa

---
+++ Basket.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projekpbov2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Basket {
+    private double speed = 10;
+    private int lives = 3;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setLives(int lives) {
+        this.lives = lives;
+    }
+
+    public int getLives() {
+        return lives;
+    }
+}
