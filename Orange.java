d00ed7de61383d55a2d12787169998f26c4b110b
azmiihniifah <168157590+azmiihniifah@users.noreply.github.com>
2024-12-07 20:13:10 +0700

    Orange.java

--- Orange.java
+++ Orange.java
@@ -1,1 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projekpbov2;
 
+/**
+ *
+ * @author ACER
+ */
+public class Orange extends Item {
+    public Orange() {
+        super("Orange", 5);
+    }
+}
