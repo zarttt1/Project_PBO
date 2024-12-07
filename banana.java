26e2c7a4ef0ab3391e57ab1c6f5251053c834119
zarttt1 <149855246+zarttt1@users.noreply.github.com>
2024-12-07 20:06:26 +0700

    Update banana.java

--- banana.java
+++ banana.java
@@ -1,1 +1,15 @@
-package projectpbov2;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projekpbov2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Banana extends Item {
+    public Banana() {
+        super("Banana", 2);
+    }
+}
