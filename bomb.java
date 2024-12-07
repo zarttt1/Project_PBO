f83ba8a14fc44a507072fe534655481b88160c21
zarttt1 <149855246+zarttt1@users.noreply.github.com>
2024-12-07 21:08:55 +0700

    Update bomb.java

--- bomb.java
+++ bomb.java
@@ -1,1 +1,16 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projekpbov2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Bomb extends Item {
+    public Bomb() {
+        super("Bomb", -1);
+    }
+}
