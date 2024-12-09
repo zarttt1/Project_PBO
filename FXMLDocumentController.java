867d8b08883b23cb5bceb67a98b1a3f18dae79f6
zarttt1 <149855246+zarttt1@users.noreply.github.com>
2024-12-09 16:57:09 +0700

    Update FXMLDocumentController.java

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1,23 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
- */
-package projekpbov2;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
-import static javafx.scene.input.KeyCode.LEFT;
-import static javafx.scene.input.KeyCode.RIGHT;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
-
-/**
- *
  * @author ACER
  */
 public class FXMLDocumentController implements Initializable {
